37342176556764a183186f5e3844bf64d7b16539
Ziyad A <zakchouc@student.42.fr>
2024-02-10 14:39:42 +0100

    add function prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,21 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *str);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_strchr(const char *s, int c);
 
 #endif
